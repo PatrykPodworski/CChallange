e6fd25a9879d08aeccc25e82d6b4831233d19650
Patryk Podworski <patryk.podworski@gmail.com>
2021-04-11 22:00:32 +0200

    Add routing and a header with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path={ROUTES.submissions}>
+          <SubmitForm />
+        </Route>
+        <Route path={ROUTES.highscores}>
+          <Highscores />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
+
+const SubmitForm = () => <div>Submit form</div>;
+const Highscores = () => <div>Highscores</div>;
+
+const ROUTES = {
+  submissions: "/submissions",
+  highscores: "/highscores",
+};
+
+const Header = () => (
+  <header>
+    <h1>C Challange</h1>
+    <nav>
+      <Link to={ROUTES.submissions}>Solve</Link>
+      <Link to={ROUTES.highscores}>Top 3</Link>
+    </nav>
+  </header>
+);
 
 export default App;
