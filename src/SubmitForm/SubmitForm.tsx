12d1735e55739842a7b6513192dab7ea13d560f8
Patryk Podworski <patryk.podworski@gmail.com>
2021-04-12 21:20:35 +0200

    Select task with a dropdown

--- src/SubmitForm/SubmitForm.tsx
+++ src/SubmitForm/SubmitForm.tsx
@@ -1,7 +1,8 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, FormikProps } from "formik";
 import React from "react";
 import Button from "../components/Button";
 import styled from "styled-components";
+import Task from "./models/Task";
 
 const SubmitForm = () => {
   const handleOnSubmit = (values: FormValues) => {
@@ -10,41 +11,60 @@ const SubmitForm = () => {
 
   const initialValues: FormValues = {
     name: "",
-    taskType: "Task1",
+    taskId: "",
     solutionCode: "",
   };
 
+  const getTaskDescription = (taskId: string) =>
+    tasksMock.find((x) => x.id === taskId)?.description;
+
   return (
     <Formik initialValues={initialValues} onSubmit={handleOnSubmit}>
-      <StyledForm>
-        <StyledInputGroup>
-          <StyledLabel htmlFor="name">Name</StyledLabel>
-          <StyledField id="name" name="name" placeholder="Name" />
-        </StyledInputGroup>
-        <StyledInputGroup>
-          <StyledLabel htmlFor="taskType">Task</StyledLabel>
-          <StyledField id="taskType" name="taskType" placeholder="Task" />
-        </StyledInputGroup>
-        <StyledInputGroup>
-          <StyledLabel>Description</StyledLabel>
-          <StyledP>{taskDescription}</StyledP>
-        </StyledInputGroup>
-        <StyledInputGroup>
-          <StyledLabel htmlFor="solutionCode">Solution code</StyledLabel>
-          <StyledField
-            id="solutionCode"
-            name="solutionCode"
-            placeholder="Solution code"
-          />
-        </StyledInputGroup>
-        <StyledInputGroup>
-          <StyledLabel />
-          <StyledButton type="submit">Submit</StyledButton>
-        </StyledInputGroup>
-      </StyledForm>
+      {(props: FormikProps<FormValues>) => (
+        <StyledForm>
+          <StyledInputGroup>
+            <StyledLabel htmlFor="name">Name</StyledLabel>
+            <StyledField id="name" name="name" placeholder="Name" />
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <StyledLabel htmlFor="taskId">Task</StyledLabel>
+            <StyledSelect
+              name="taskId"
+              value={props.values.taskId}
+              onBlur={() => props.setFieldTouched("taskId", true)}
+              onChange={(e) => props.setFieldValue("taskId", e.target.value)}
+            >
+              <option key={"default"} value="" label="Select a task" />
+              {tasksMock.map((x) => (
+                <option key={x.id} value={x.id} label={x.name} />
+              ))}
+            </StyledSelect>
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <StyledLabel>Description</StyledLabel>
+            <StyledP>{getTaskDescription(props.values.taskId)}</StyledP>
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <StyledLabel htmlFor="solutionCode">Solution code</StyledLabel>
+            <StyledField
+              id="solutionCode"
+              name="solutionCode"
+              placeholder="Solution code"
+            />
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <StyledLabel />
+            <StyledButton type="submit">Submit</StyledButton>
+          </StyledInputGroup>
+        </StyledForm>
+      )}
     </Formik>
   );
 };
+
+const StyledSelect = styled.select`
+  flex-grow: 1;
+`;
 
 const StyledP = styled.p`
   margin: 0;
@@ -77,12 +97,28 @@ const StyledForm = styled(Form)`
 
 type FormValues = {
   name: string;
-  taskType: TaskType;
+  taskId: string;
   solutionCode: string;
 };
-
-type TaskType = "Task1" | "Task2";
 
 const taskDescription = `Meoooow meowing chowing and wowing purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table eat owner's food or catch eat throw up catch eat throw up bad birds weigh eight pounds but take up.`;
 
 export default SubmitForm;
+
+const tasksMock: Task[] = [
+  {
+    id: "task1 id",
+    name: "task1 name",
+    description: `task1 ${taskDescription}`,
+  },
+  {
+    id: "task2 id",
+    name: "task2 name",
+    description: `task2 ${taskDescription}`,
+  },
+  {
+    id: "task3 id",
+    name: "task3 name",
+    description: `task3 ${taskDescription}`,
+  },
+];
---
+++ src/SubmitForm/models/Task.ts
@@ -0,0 +1,7 @@
+type Task = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+export default Task;
